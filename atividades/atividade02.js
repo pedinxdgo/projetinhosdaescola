676527cbc7ce200cccb24ff8782cb600c3c440a6
pedinxdgo <pedinxdgo@hotmail.com>
2026-03-26 11:44:52 -0300

    Atividade 02

---
+++ atividades/atividade02.js
@@ -0,0 +1,4 @@
+var idadecachorro = 6;
+var idade = idadecachorro * 7
+console.log(`7 anos de um cachorro equivale a 1 ano de vida humana, portanto a 
+idade humana ${idade} é o mesmo que ${idadecachorro} anos de um cachorro`);
