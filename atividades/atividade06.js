027f3c886fad1999a514f3e35835b3f252dad247
pedinxdgo <pedinxdgo@hotmail.com>
2026-03-27 11:55:19 -0300

    Atividade 06

--- atividades/atividade06.js
+++ atividades/atividade06.js
@@ -2,7 +2,10 @@ let nota1 = 7.5;
 let nota2 = 8.0;
 let media = (nota1 + nota2) / 2;
 
-if(media >= 7.0){
+if(nota1>10 || nota1<0 || nota2 > 10 || nota2<0){
+    console.log("Dados inválidos");
+}
+else if(media >= 7.0){
     console.log("Aprovado");
 }
 else if(media >= 5.0 && media < 7.0){
