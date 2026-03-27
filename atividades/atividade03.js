d0fb1d83e34de32816cdee0226e3d1366f8478cc
pedinxdgo <pedinxdgo@hotmail.com>
2026-03-27 10:41:08 -0300

    Atividade 03

--- atividades/atividade03.js
+++ atividades/atividade03.js
@@ -2,3 +2,10 @@ var idade = 18;
 var conviteVIP = false;
 var entrada = idade >=18 && conviteVIP == true ? "Entrada Autorizada!" : "Entrada Não Autorizada!";
 console.log(entrada);
+
+if(idade >=18 && conviteVIP == true){
+    console.log("Entrada Autorizada!");
+}
+else{
+    console.log("Entrada Não Autorizada!");
+}
