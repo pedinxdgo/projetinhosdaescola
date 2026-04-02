f9489960d8b5ed6680350a719efe26ee2648d977
pedinxdgo <pedinxdgo@hotmail.com>
2026-04-02 12:17:17 -0300

    Atividade 11

---
+++ atividades/atividade11.js
@@ -0,0 +1,7 @@
+let nomes = ["Pedro Potrick", "Miguel", "Emanuel", "Felipao", "Nicoly", "Henrique", "Saymon", "Everson", "Sther", "Alex"];
+if(nomes.includes("Pedro Potrick") == true){
+    console.log("Usuário Localizado no Sistema");
+}
+else{
+    console.log("Usuário Não Localizado");
+}
