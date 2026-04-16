209d3e9ad216545362993b2c9b13ef6b10c354c3
pedinxdgo <pedinxdgo@hotmail.com>
2026-04-16 10:39:16 -0300

    Desafio Final Concluído

--- atividades/atividadeFinal.js
+++ atividades/atividadeFinal.js
@@ -1,12 +1,12 @@
-let estoque = ["SSD 1 Terabyte", "Nvidia geForce RTX 4060", "AMD Ryzen 5 5600", "Memoria RAM DDR4 8GB", "Placa Mãe MSI A520M"];
+let estoque = ["SSD NVME 1 Terabyte", "Nvidia geForce RTX 4060", "AMD Ryzen 5 5600", "Memoria RAM DDR4 8GB", "Placa Mãe MSI A520M"];
 estoque.push("Fonte de Alimentação MSI MAG 600W");
 let clienteNome = "Zeca Tatu";
-let clienteIdade = 17;
+let clienteIdade = 18;
 let possuiCupom = false;
-let valorCompra = 550;
+let valorCompra = 500;
 
-function processarVenda(valorCompra, possuiCupom){
-    let valorFinal = valorCompra > 500 || possuiCupom == true ? valorCompra * 0.85 : valorCompra;
+function processarVenda(valorTotal, cupoAtivo){
+    let valorFinal = valorTotal > 500 || cupoAtivo == true ? valorTotal * 0.85 : valorTotal;
     return (valorFinal);
 }
 
@@ -15,14 +15,15 @@ if(clienteIdade >= 16){
     console.log(`Venda autorizada para ${clienteNome}!`);
     let venda = processarVenda(valorCompra, possuiCupom);
 
-    for(let i = 0; i < estoque.length; i++){
-        console.log(`Despachando item: ${estoque[i]}`);
-        estoque.splice(0, 1);
+    while(estoque.length > 5){
+        console.log(`Despachando item: ${estoque[0]}`);
+        estoque.shift();
     } 
-    console.log(`Relatório da Loja: Cliente ${clienteNome} processou um pedido de R$ ${venda}. \nItens restantes no estoque: ${estoque.length}`);
+    console.log(`Relatório da Loja: Cliente ${clienteNome} processou um pedido de R$ ${venda.toFixed(2)}. \nItens restantes no estoque: ${estoque.length}`);
+    console.log(`-> ${estoque.join(" - ")} <-`);
 
 }
 else{
     console.log("Venda bloqueada. Idade insuficiente!");
 }
-console.log(estoque);
+
